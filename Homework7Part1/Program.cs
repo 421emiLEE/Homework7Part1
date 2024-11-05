eb2b141bea49a7f0280bebfefbeeef411c667961
Emily Sierra <sierraemely@gmail.com>
2024-11-04 17:57:44 -0800

    Add project files.

---
+++ Homework7Part1/Program.cs
@@ -0,0 +1,18 @@
+﻿using Homework7Part1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        odometer trip1 = new odometer();
+        trip1.add(50, 10);
+        Console.WriteLine(trip1.toString());
+        Console.WriteLine("Fuel used for trip1 is " + trip1.MPG());
+
+        odometer trip2 = new odometer(100,20);
+        Console.WriteLine("Fuel used for trip2 is " + trip2.MPG());
+        trip1.reset();
+        Console.WriteLine(trip1.toString());
+
+    }
+}
---
+++ Homework7Part1/odometer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework7Part1
+{
+    internal class odometer
+    {
+        private int miles;
+        private double fuelUsed;
+
+
+        public odometer() { miles = 0; fuelUsed = 0.0; }
+
+        public odometer(int _miles, double _fuelUsed)
+        {
+            miles = _miles;
+            fuelUsed = _fuelUsed;
+        }
+
+        public int Miles { get => miles; set => miles = value; }
+        public double FuelUsed { get => fuelUsed; set => fuelUsed = value; }
+        //methods
+        public void reset()
+        {
+            Miles = 0;
+            FuelUsed = 0.0;
+        }
+
+        public void add(int m, double f)
+        {
+            Miles += m; //Miles = Miles + m;
+
+            FuelUsed += f; //FuelUsed = FuelUsed + f;
+        }
+        public double MPG()
+        {
+            return Miles / FuelUsed;
+        }
+
+        public String toString()
+        {
+            return "" + Miles + " miles driven " + FuelUsed + " Fuel Used";
+        }
+    }
+}
